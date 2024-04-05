cb6fee25aa67573eca2dfc0d04aa6938312e4f55
Amol Mane <103019607+amolmane6999@users.noreply.github.com>
2024-04-05 16:44:58 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,55 @@
+﻿let current = ""
+let  previous = ""
+let numbers = document.getElementsByClassName("data-number")
+let operations = document.getElementsByClassName("data-operation")
+
+Array.from(numbers).forEach((number) => {
+    number.addEventListener('click', (e) => {
+        console.log(e.target.innerHTML)
+        current = current + e.target.innerHTML;
+        console.log(current)
+        var output = document.getElementById("current")
+        console.log(output)
+
+        output.innerHTML=current
+    });
+});
+
+Array.from(operations).forEach((operation) => {
+    operation.addEventListener('click', (e) => {
+        console.log(e.target.innerHTML)
+        if (e.target.innerHTML == '÷') {
+            current = current + '/'
+            document.getElementById("current").innerHTML = current
+        }
+        else {
+            current = current + e.target.innerHTML;
+            document.getElementById("current").innerHTML = current
+        }
+    });
+});
+    document.getElementById("data-all-clear").addEventListener('click', () => {
+        current = " "
+        previous = " "
+        document.getElementById("current").innerHTML = current
+        document.getElementById("previous").innerHTML = previous
+
+
+    });
+
+
+document.getElementById("data-delete").addEventListener('click', () => {
+    current = current.toString().slice(0, -1)
+    document.getElementById("current").innerHTML = current
+
+});
+
+document.getElementById("data-equals").addEventListener('click', () => {
+    current = eval(current)
+    previous = current
+    document.getElementById("current").innerHTML = current
+
+    document.getElementById("previous").innerHTML = previous
+
+
+});
